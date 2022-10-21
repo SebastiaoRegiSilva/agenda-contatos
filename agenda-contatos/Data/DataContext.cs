7953f0114771bf79cea82430549921b07c76fa01
Sebastião Willian <swillian2009@gmail.com>
2022-10-20 23:50:25 -0300

    Criação da base de dados.

--- agenda-contatos/Data/DataContext.cs
+++ agenda-contatos/Data/DataContext.cs
@@ -1,11 +1,24 @@
-﻿using System;
+﻿using Agenda.Contatos.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Agenda.Contatos.Data
 {
-    public class DataContext
+    public class DataContext : DbContext
     {
+        /// <summary>
+        /// Construtor com injeção de depedência.
+        /// </summary>
+        /// <param name="options"></param>
+        public DataContext(DbContextOptions<DataContext> options) : base (options)
+        {
+
+        }
+
+        public DbSet<ContatoModel> Contatos { get; set; }
+
     }
 }
