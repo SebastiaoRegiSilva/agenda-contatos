6387b27d23c3e228aa100d257eff21fb2aa98a1e
Sebastião Willian <swillian2009@gmail.com>
2023-02-13 22:10:41 +0000

    Breves correções, ainda não testadas na migration.

--- agenda-contatos/Migrations/20221022145710_CriandoTabelaContatos.cs
+++ agenda-contatos/Migrations/20221022145710_CriandoTabelaContatos.cs
@@ -6,18 +6,19 @@ namespace Agenda.Contatos.Migrations
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
+            /// Código de identificação, nome e número de telefone não podem ser nulos. 
             migrationBuilder.CreateTable(
                 name: "Contatos",
                 columns: table => new
                 {
                     Id = table.Column<int>(type: "int", nullable: false)
                         .Annotation("SqlServer:Identity", "1, 1"),
-                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: false),
                     Email = table.Column<string>(type: "nvarchar(max)", nullable: true),
-                    NumeroCelular = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    NumeroCelular = table.Column<string>(type: "nvarchar(max)", nullable: false),
                     Pais = table.Column<string>(type: "nvarchar(max)", nullable: true),
                     Estado = table.Column<string>(type: "nvarchar(max)", nullable: true),
-                    Tipo = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                    Tipo = table.Column<TipoContatoModel>(type: "nvarchar(max)", nullable: true)
                 },
                 constraints: table =>
                 {
