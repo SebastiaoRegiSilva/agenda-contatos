cbaf82aa552c591e4f9d4a4e1af50d64c6730f9f
Sebastião Willian <swillian2009@gmail.com>
2023-02-22 21:25:06 +0000

    Criando uma model do tipo de contato.

---
+++ agenda-contatos/Models/TipoContatoModel.cs
@@ -0,0 +1,18 @@
+﻿namespace Agenda.Contatos.Models
+{
+    /// <summary>
+    /// Modelo de tipo do contato a ser salvo na base dados.
+    /// </summary>
+    public class TipoContatoModel
+    {
+        /// <summary>
+        /// Código de identificação do tipo do contato.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Nome do tipo do contato.
+        /// </summary>
+        public string Nome { get; set; }
+    }
+}
