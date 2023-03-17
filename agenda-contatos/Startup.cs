5fb169a5a0fa05c7941a60510788150c4f99f5ed
Sebastião Willian <swillian2009@gmail.com>
2023-03-17 23:40:08 +0000

    Injeção de dependência, tipo de contato adicionado.

--- agenda-contatos/Startup.cs
+++ agenda-contatos/Startup.cs
@@ -28,6 +28,7 @@ namespace Agenda.Contatos
             // Injeção de dependência.
             services.AddScoped<IContatoRepository, ContatoRepository>();
             services.AddScoped<IUsuarioRepository, UsuarioRepository>();
+            services.AddScoped<ITipoContatoRepository, TipoContatoRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
