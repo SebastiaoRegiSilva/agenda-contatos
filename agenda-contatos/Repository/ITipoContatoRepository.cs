69c91a219cbb9f35d3c311b5471a0a04081038bd
Sebastião Willian <swillian2009@gmail.com>
2023-03-17 22:41:43 +0000

    Implementação da interface que padroniza os tipos de contato.

--- agenda-contatos/Repository/ITipoContatoRepository.cs
+++ agenda-contatos/Repository/ITipoContatoRepository.cs
@@ -1,11 +1,34 @@
-﻿using System;
+﻿using Agenda.Contatos.Models;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Agenda.Contatos.Repository
 {
+    /// <summary>Interface que padroniza o repositório dos tipos de contato.</summary>
     interface ITipoContatoRepository
     {
+        /// <summary>
+        /// Cadastra no repositório um tipo de contato.
+        /// </summary>
+        /// <param name="tipoContato">Modelo de um tipo de contato.</param>
+        TipoContatoModel CadastrarTipoContato(TipoContatoModel tipoContato);
+
+        /// <summary>
+        /// Apaga no repositório um tipo de contato com base em seu código de identificação.
+        /// </summary>
+        /// <param name="id">Código de identificação de um tipo de contato.</param>
+        /// <returns>Sim ou Não para a boa sucessão da operação na base de dados.</returns>
+        bool ApagarTipoContato(int id);
+
+        /// <summary>
+        /// Busca no repositório todos os tipo de contato cadastrados.
+        /// </summary>
+        /// <returns>Lista de tipos de contato.</returns>
+        List<TipoContatoModel> BuscarTodosTipoContatos();
+
+        /// <summary>
+        /// Edita no repositório um tipo de contato.
+        /// </summary>
+        /// <param name="tipoContato">Modelo de um tipo de contato.</param>
+        TipoContatoModel EditarTipoContato(TipoContatoModel tipoContato);
     }
 }
