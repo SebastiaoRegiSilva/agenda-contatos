ed9fa176621d64c6f13282d43bde65960f402c11
Sebastião Willian <swillian2009@gmail.com>
2023-03-18 10:27:06 +0000

    Documentação da interface de usuário.

--- agenda-contatos/Repository/IUsuarioRepository.cs
+++ agenda-contatos/Repository/IUsuarioRepository.cs
@@ -6,16 +6,40 @@ using System.Threading.Tasks;
 
 namespace Agenda.Contatos.Repository
 {
+    /// <summary>Interface que padroniza o repositório de usuários.</summary>
     public interface IUsuarioRepository
     {
-        UsuarioModel BuscarPorId(int id);
+        /// <summary>
+        /// Busca no repositório um usuário com base em seu código de identificação.
+        /// </summary>
+        /// <param name="id">Código de identificação do usuário.</param>
+        /// <returns>Usuário recuperado.</returns>
+        UsuarioModel BuscarUsuarioPorId(int id);
 
-        List<UsuarioModel> BuscarTodos();
+        /// <summary>
+        /// Busca todos usuários cadastrados no repositório.
+        /// </summary>
+        /// <returns>Todos usuários cadastrados.</returns>
+        List<UsuarioModel> BuscarTodosUsuarios();
 
-        UsuarioModel Cadastrar(UsuarioModel usuario);
+        /// <summary>
+        /// Cadastra no repositório um usuário.
+        /// </summary>
+        /// <param name="usuario">Modelo de um usuário.</param>
+        /// <returns></returns>
+        UsuarioModel CadastrarUsuario(UsuarioModel usuario);
 
+        /// <summary>
+        /// Edita no repositório um usuário.
+        /// </summary>
+        /// <param name="usuario">Modelo de um usuário.</param>
         UsuarioModel EditarUsuario(UsuarioModel usuario);
 
+        /// <summary>
+        /// Apaga no repositório um usuário com base em seu código de identificação.
+        /// </summary>
+        /// <param name="id">Código de identificação do usuário.</param>
+        /// <returns>Confirmação ou não da boa sucessão da operação.</returns>
         bool ApagarUsuario(int id);
     }
 }
