09b1003b52903e953b09cb8d4dde6b857700b29b
Sebastião Willian <swillian2009@gmail.com>
2023-05-20 16:59:44 +0100

    Preparando a aplicação para entrar no início na página de login.

--- agenda-contatos/Startup.cs
+++ agenda-contatos/Startup.cs
@@ -55,7 +55,7 @@ namespace Agenda.Contatos
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Login}/{action=Index}/{id?}");
             });
         }
     }
