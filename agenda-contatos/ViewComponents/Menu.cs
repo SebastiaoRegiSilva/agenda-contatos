1c2757aeb0ee1588d66eae31120e8f3e1e3ba849
Sebastião Willian <swillian2009@gmail.com>
2023-05-21 17:08:19 +0100

    Implementação completa para uso de sessões de usuários com o botão de saída.

---
+++ agenda-contatos/ViewComponents/Menu.cs
@@ -0,0 +1,27 @@
+﻿using Agenda.Contatos.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
+
+namespace Agenda.Contatos.ViewComponents
+{
+    public class Menu : ViewComponent
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns>View que está em Components/Menu/Default.</returns>
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            string userSession = HttpContext.Session.GetString("sessaoUsuarioLogado");
+
+            if (string.IsNullOrEmpty(userSession))
+                return null;
+
+            UsuarioModel usuarioLogado = JsonConvert.DeserializeObject<UsuarioModel>(userSession);
+
+            return View(usuarioLogado);
+        }
+    }
+}
