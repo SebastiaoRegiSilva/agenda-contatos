2d04f35e3cf8b168784a7166aea374f25e2830b2
Sebastião Willian <swillian2009@gmail.com>
2023-09-18 18:52:04 +0100

    Implementação de classe e interface de email para recuperação de senhas.

---
+++ agenda-contatos/Helper/Email.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Net;
+using System.Net.Mail;
+
+namespace Agenda.Contatos.Helper
+{
+    //todo Implementar logs nas operações com emails.
+    public class Email : IEmail
+    {
+        /// <summary>
+        /// Injeção de depedência que fornece acesso às configurações no arquivo appsettings.json.
+        /// </summary>
+        private readonly IConfiguration _configuration;
+
+        /// <summary>
+        /// Construtor com parâmetros para inicialização.
+        /// </summary>
+        /// <param name="configuration">Injeção de dependência.</param>
+        public Email(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        /// <summary>
+        /// Enviar e-mails.
+        /// </summary>
+        /// <param name="endEmail">Endereço do receptor.</param>
+        /// <param name="assunto">Assunto do e-mail.</param>
+        /// <param name="mensagem">Mensagem informativa.</param>
+        /// <returns>Efetuado com sucesso ou não.</returns>
+        public bool EnviarEmail(string endEmail, string assunto, string mensagem)
+        {
+            try
+            {
+                string userName = _configuration.GetValue<string>("SMTP:UserName");
+                string nome = "Authentication Service ";
+                string host = _configuration.GetValue<string>("SMTP:Host");
+                string senha = _configuration.GetValue<string>("SMTP:Senha");
+                int porta = _configuration.GetValue<int>("SMTP:Porta");
+
+                MailMessage mail = new MailMessage()
+                {
+                    From = new MailAddress(userName, nome),
+                };
+
+                mail.To.Add(endEmail);
+                mail.Subject = assunto;
+                mail.Body = mensagem;
+                mail.IsBodyHtml = true;
+                mail.Priority = MailPriority.High;
+
+                using (SmtpClient smtp = new SmtpClient(host, porta))
+                {
+                    smtp.Credentials = new NetworkCredential(userName, senha);
+                    smtp.EnableSsl = true;
+                    smtp.Send(mail);
+                    return true;
+                }
+            }
+            catch (Exception)
+            {
+                // Implementar logs para registro de eventuais erros de envios.
+                return false;
+            }
+        }
+    }
+}
---
+++ agenda-contatos/Helper/IEmail.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Agenda.Contatos.Helper
+{
+    /// <summary>
+    /// Interface que padroniza a gestão de envio de e-mails.
+    /// </summary>
+    public interface IEmail
+    {
+        /// <summary>
+        /// Enviar e-mail.
+        /// </summary>
+        /// <param name="endEmail">Endereço que receberá o e-mail.</param>
+        /// <param name="assunto">Assunto do e-mail.</param>
+        /// <param name="mensagem">Conteúdo do e-mail.</param>
+        bool EnviarEmail(string endEmail, string assunto, string mensagem);
+
+    }
+}
