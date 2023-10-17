0dfb87499ec409eb869037f9b89fd90653b77a7a
Sebastião Willian <swillian2009@gmail.com>
2023-10-17 21:48:14 +0100

    Criando controller e model de alteração de senhas.

--- agenda-contatos/Controllers/NovaSenhaController.cs
+++ agenda-contatos/Controllers/NovaSenhaController.cs
@@ -1,16 +1,62 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Agenda.Contatos.Helper;
+using Agenda.Contatos.Models;
+using Agenda.Contatos.Repository;
+using Microsoft.AspNetCore.Mvc;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Agenda.Contatos.Controllers
 {
     public class NovaSenhaController : Controller
     {
+        // Injeção de depedência...
+        private readonly IUsuarioRepository _usuarioRepository;
+
+        // Injeção de depedência...
+        private readonly ISession _session;
+
+        /// <summary>
+        /// Construtor com parâmetros para inicialização.
+        /// </summary>
+        /// <param name="usuarioRepository"></param>
+        /// <param name="session"></param>
+        public NovaSenhaController(IUsuarioRepository usuarioRepository, ISession session)
+        {
+            _usuarioRepository = usuarioRepository;
+            _session = session;
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        /// <summary>
+        /// Alterar senha de usuário, a partir de inserção de senha atual.
+        /// </summary>
+        /// <param name="alterarSenhaModel">Objeto de alteração de senha</param>
+        [HttpPost]
+        public IActionResult AlterarSenha(AlterarSenhaModel alterarSenhaModel)
+        {
+            try
+            {
+                UsuarioModel usuarioLogado = _session.BuscarSessaoUsuario();
+                alterarSenhaModel.Id = usuarioLogado.Id;
+
+                if(ModelState.IsValid)
+                {
+                    _usuarioRepository.AlterarSenha(alterarSenhaModel);
+
+                    TempData["MensagemSucesso"] = $"Senha alterada com sucesso!";
+                    return View("Index", alterarSenhaModel);
+                }
+                
+                return View("Index", alterarSenhaModel);
+            }
+            catch (Exception error)
+            {
+                TempData["MensagemErro"] = $"Erro na alteração da senha! ERRO =>{error.Message}";
+                return RedirectToAction("Index", alterarSenhaModel);
+            }
         }
     }
 }
---
+++ agenda-contatos/Models/AlterarSenhaModel.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Agenda.Contatos.Models
+{
+    /// <summary>
+    /// Modelo de uma alteração de senha a ser salva na base dados.
+    /// </summary>
+    public class AlterarSenhaModel
+    {
+        /// <summary>
+        /// Código de identificação do usuário que terá senha alterada.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Senha do usuário de acesso ao sistema.
+        /// Implementar melhorias, criptografar, SALT.
+        /// </summary>
+        [Required(ErrorMessage = "Digite a senha atual do usuário")]
+        
+        public string SenhaAtual { get; set; }
+        /// <summary>
+        /// Nova senha do usuário de acesso ao sistema.
+        /// </summary>
+        [Required(ErrorMessage = "Digite nova senha do usuário")]
+        public string NovaSenha { get; set; }
+
+        /// <summary>
+        /// Confirmar nova senha do usuário de acesso ao sistema.
+       /// </summary>
+        [Required(ErrorMessage = "Confirme a nova senha do usuário")]
+        [Compare("NovaSenha", ErrorMessage = "Senhas não são iguais!")]
+        public string ConfirmarSenha { get; set; }
+    }
+}
